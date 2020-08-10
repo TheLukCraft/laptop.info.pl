c6fb1b1355418e49547ce94773e3d25731561df8
TheLukCraft <luk1997r@o2.pl>
2020-08-10 11:28:56 +0200

    Initial commit

---
+++ Navbar/homeNav.php
@@ -0,0 +1,21 @@
+  <!-- NAVBAR -->
+  <nav>
+        <div class="nav-wrapper grey darken-3">
+          <a href="#!" class="brand-logo">Laptop</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="contactForm.php" class="waves-effect waves-light btn"><i class="material-icons left">laptop_mac</i>Darmowa Pomoc</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">Javascript</a></li>
+            <li><a href="mobile.html">Mobile</a></li>
+          </ul>
+        </div>
+      </nav>
+    <!-- mobile navbar -->
+      <ul class="sidenav grey lighten-3" id="mobile-demo">
+        <li><a href="contactForm.php" class="waves-effect waves-light btn"><i class="material-icons left">laptop_mac</i>Darmowa Pomoc</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+      <!-- END NAVBAR -->
---
+++ index.php
@@ -0,0 +1,284 @@
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Darmowa pomoc w wyborze laptopa</title>
+          <!-- link to Stylesheets -->
+          <link rel="stylesheet" type="text/css" href="Stylesheets/main.css">
+       <!--Import Google Icon Font-->
+       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+       <!--Import materialize.css-->
+       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+          <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <!-- Jquery link -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157446144-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-157446144-1');
+      </script>
+
+
+</head>
+<body class="grey lighten-5">
+  <!-- COOKIES -->
+<div id="simplecookienotification_v01" style="display: block; z-index: 99999; min-height: 35px; width: 100%; position: fixed; background: rgb(43, 54, 67) none repeat scroll 0% 0%; border-image: none 100% / 1 / 0 stretch; border-top: 1px solid rgb(160, 178, 192); text-align: center; right: 0px; color: rgb(119, 119, 119); bottom: 0px; left: 0px; border-color: rgb(160, 178, 192); box-shadow: rgba(0, 0, 0, 0.8) 0px 0px 4px 1px;">
+  <div style="padding:10px; margin-left:15px; margin-right:15px; font-size:14px; font-weight:normal;">
+  <span id="simplecookienotification_v01_powiadomienie">Ta strona używa plików cookie w celu usprawnienia i ułatwienia dostępu do serwisu oraz prowadzenia danych statystycznych. Dalsze korzystanie z tej witryny oznacza akceptację tego stanu rzeczy.</span><span id="br_pc_title_html"><br></span>
+  <a id="simplecookienotification_v01_polityka" href="http://www.info.pl/politics.php" style="color: rgb(160, 178, 192);">Polityka Prywatności</a><span id="br_pc2_title_html"> &nbsp;&nbsp; </span>
+  <a id="simplecookienotification_v01_info" href="http://jakwylaczyccookie.pl/jak-wylaczyc-pliki-cookies/" style="color: rgb(160, 178, 192);">Jak wyłączyć cookies?</a><span id="br_pc3_title_html"> &nbsp;&nbsp; </span>
+  <a id="simplecookienotification_v01_info2" href="https://nety.pl/cyberbezpieczenstwo" style="color: rgb(160, 178, 192);">Cyberbezpieczeństwo</a><div id="jwc_hr1" style="height: 10px; display: none;"></div>
+  <a id="okbutton" href="javascript:simplecookienotification_v01_create_cookie('simplecookienotification_v01',1,7);" style="position: absolute; background: rgb(160, 178, 192) none repeat scroll 0% 0%; color: rgb(255, 255, 255); padding: 5px 15px; text-decoration: none; font-size: 12px; font-weight: normal; border: 0px solid rgb(43, 54, 67); border-radius: 0px; top: 5px; right: 5px;">AKCEPTUJĘ</a><div id="jwc_hr2" style="height: 10px; display: none;"></div>
+  </div>
+  </div>
+  <script type="text/javascript">var galTable= new Array(); var galx = 0;</script><script type="text/javascript">function simplecookienotification_v01_create_cookie(name,value,days) { if (days) { var date = new Date(); date.setTime(date.getTime()+(days*24*60*60*1000)); var expires = "; expires="+date.toGMTString(); } else var expires = ""; document.cookie = name+"="+value+expires+"; path=/"; document.getElementById("simplecookienotification_v01").style.display = "none"; } function simplecookienotification_v01_read_cookie(name) { var nameEQ = name + "="; var ca = document.cookie.split(";"); for(var i=0;i < ca.length;i++) { var c = ca[i]; while (c.charAt(0)==" ") c = c.substring(1,c.length); if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length); }return null;}var simplecookienotification_v01_jest = simplecookienotification_v01_read_cookie("simplecookienotification_v01");if(simplecookienotification_v01_jest==1){ document.getElementById("simplecookienotification_v01").style.display = "none"; }</script>
+  <!-- END COOKIES -->
+    <!-- NAVBAR -->
+<?php
+include_once 'Navbar/homeNav.php';
+?>
+      <!-- END NAVBAR -->
+      <!-- PARALLAX -->
+      <div class="parallax-container">
+        <div class="parallax"><img src="img/intro.jpg" style="width: 110%;"></div>
+      </div>
+      <div class="section white">
+        <div class="row container">
+           <!-- GRID -->
+    <div class="row">
+      <!-- START FIRST S12 -->
+      <div class="col s12">
+    <!-- TEXT with BUTTON -->
+    <br><br>
+    <h1 class="header center grey-text text-darken-4">Darmowa pomoc</h1>
+    <div class="row center">
+      <h5 class="header col s12 light">Chcesz zakupić nowy laptop, ale nie wiesz jaki? Pomagam <span class="red-text accent-4 "><b>za darmo!</b></span>
+      Kliknij poniżej, aby dowiedzieć się więcej ;)</h5>
+    </div>
+    <div class="row center">
+      <a href="" id="download-button" class="btn-large waves-effect waves-light grey darken-4 pulse">Zaczynamy ;)</a>
+    </div>
+    <br><br>
+    <!-- END TEXT with BUTTON -->
+    <!-- END FIRST S12 -->
+      </div>
+        </div>
+      </div>
+      </div>
+      <div class="parallax-container">
+        <div class="parallax"><img src="img/intro4.jpg" style="width: 110%;"></div>
+      </div>
+      <br><br>
+      <!-- END PARALLAX -->
+      <div class="content">
+      
+      <!-- YOUTUBE -->
+      <div class="row grey lighten-5">
+        <div class="col s12 center-align">
+      <iframe class="youtube" width="780" height="472" src="https://www.youtube.com/embed/aWC21-XDryg" frameborder="0"
+       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      </div>
+       <!-- END YOUTUBE -->
+       <br><br>
+
+    <!-- CARDS of laptops  -->
+    <!-- START FIRST S4 xl4 -->
+    <div class="row">
+        <div class="col s12 m6 xl4 ">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/laptop.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>Szukasz laptopa do biurowych/domowych zastosowań? Świetnie! Ten laptop spełni
+                  wszystkie Twoje oczekiwania za tą kwotę. Dobry procesor, świetnej jakości obraz,
+                  dedykowana karta graficzna, dzięki, której laptop nie będzie zamulać
+                  w programach codziennego użytku.
+              </p>
+            </div>
+          </div>
+        </div>
+
+      
+      <!-- END FIRST S4 XL4 -->
+         <!-- START FIRST S4 -->
+        <div class="col s12 m6 xl4">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/laptop.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>Szukasz laptopa do biurowych/domowych zastosowań? Świetnie! Ten laptop spełni
+                  wszystkie Twoje oczekiwania za tą kwotę. Dobry procesor, świetnej jakości obraz,
+                  dedykowana karta graficzna, dzięki, której laptop nie będzie zamulać
+                  w programach codziennego użytku.
+              </p>
+            </div>
+          </div>
+        </div>
+      <!-- END FIRST S12 XL4 -->
+         <!-- START FIRST S4 -->
+        <div class="col s12 m6 xl4">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/laptop.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>Szukasz laptopa do biurowych/domowych zastosowań? Świetnie! Ten laptop spełni
+                  wszystkie Twoje oczekiwania za tą kwotę. Dobry procesor, świetnej jakości obraz,
+                  dedykowana karta graficzna, dzięki, której laptop nie będzie zamulać
+                  w programach codziennego użytku.
+              </p>
+            </div>
+          </div>
+         <!-- END ROW -->
+        </div>
+      <!-- END FIRST S12 XL4 -->
+         <!-- START FIRST S4 -->
+         <div class="col s12 m6 xl4">
+          <div class="card">
+            <div class="card-image">
+              <img src="img/laptop.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>Szukasz laptopa do biurowych/domowych zastosowań? Świetnie! Ten laptop spełni
+                  wszystkie Twoje oczekiwania za tą kwotę. Dobry procesor, świetnej jakości obraz,
+                  dedykowana karta graficzna, dzięki, której laptop nie będzie zamulać
+                  w programach codziennego użytku.
+              </p>
+            </div>
+          </div>
+         <!-- END ROW -->
+        </div>
+      <!-- END FIRST S12 XL4 -->
+      </div>
+      <!-- END CARDS -->
+    
+    <!-- INFORMATION -->
+    <!-- START SECOND S12 -->
+    <div class="col s12">
+    <div class="section">
+
+        <!--   Icon Section   -->
+        <div class="row" style="padding-left: 10%; padding-right: 10%; margin: 0;">
+          <div class="col s12 m4 information">
+            <div class="icon-block">
+              <h2 class="center light-blue-text"><i class="material-icons">person</i></h2>
+              <h5 class="center">Kim jestem?</h5>
+  
+              <p class="light">Jestem Łukasz Capała, prowadzę kanał na youtubie od 2013r. Jestem po technikum Informatycznym.
+                   Aktualnie studiuję informatykę na 3-cim roku. Rok temu pracowałem jako promotor w sklepie Media Markt,
+                    czyli doradzałem po prostu w zakupach.</p>
+            </div>
+          </div>
+  
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center light-blue-text"><i class="material-icons">monetization_on</i></h2>
+              <h5 class="center">Dlaczego za darmo?</h5>
+  
+              <p class="light"> Robię to za darmo, gdyż płaci mi firma za jakikolwiek sprzedany produkt na sklepie x-kom przez internet.
+                   Dzięki temu jestem bestronny jeżeli chodzi o marki laptopów. To jest powiedzmy takie drugie kieszonkowe, bo kokosów
+                    z tego nie ma. Oczywiście możesz zakupić produkt gdziekolwiek chcesz. To będzie tylko <b>DOBRA WOLA</b>, jeżeli kupisz z mojego linku produkt lub np. wpłacisz napiwek.
+                      <span class="red-text accent-4">Kliknij na mnie</span> </p>
+            </div>
+          </div>
+  
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center light-blue-text"><i class="material-icons">create</i></h2>
+              <h5 class="center">Co mam zrobić?</h5>
+  
+              <p class="light">Wystarczy, że wejdziesz w <span class="">ankietę</span> i uzupełnisz ją według swoich preferencji. Pomoże mi to w 
+            szybszym doborze odpowiedniego laptopa dla Ciebie. Czas oczekiwania, to 48h, ale to zależy od moich zajęć na studiach, ilości e-maili. </p>
+            </div>
+          </div>
+        </div>
+  
+      </div>
+      <br><br>
+    </div>
+      <!-- END GRID -->
+    </div>
+    <!-- END INFORMATION -->
+      <!-- Content closer -->
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="page-footer grey darken-3">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Kontakt</h5>
+              <span class="grey-text text-lighten-4 footerText">
+                <div class="footerText">
+              <li>Łukasz Capała</li>
+              <li>+48 667 711 900</li>
+              <li>thelukcraft@gmail.com</li>
+              <li><a href="https://www.thelukcraft.pl">www.thelukcraft.pl</a></li>
+              </div>
+            </span>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Links</h5>
+              <ul>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+          © 2020 Copyright Łukasz Capała
+          <a class="grey-text text-lighten-4 right" href="#!">Informacje prawne</a>
+          </div>
+        </div>
+      </footer>
+      <!-- END FOOTER -->
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <!-- navbar script -->
+      <script>
+      $(document).ready(function(){
+        $('.sidenav').sidenav();
+      });
+      </script>
+      <!-- Carousel script -->
+      <script>
+        $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+  
+      </script>
+      <!-- Parallax script -->
+      <script>
+      $(document).ready(function(){
+        $('.parallax').parallax();
+      });
+      </script> 
+
+      <!-- PreLoader scripts -->
+      <script>
+      document.querySelector('.img').style.display="none";
+      </script>
+</body>
+</html>
